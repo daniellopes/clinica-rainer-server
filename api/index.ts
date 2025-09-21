d7f32df0ba2ba3117a04c7cd84436951ee42df4d
System Administrator <dl.lopes2011@gmail.com>
2025-09-21 09:27:23 -0300

    Fix: rebuild project

--- api/index.ts
+++
@@ -1,6 +0,0 @@
-import app from '../src/app';
-import { VercelRequest, VercelResponse } from '@vercel/node';
-
-export default function handler(req: VercelRequest, res: VercelResponse) {
-  return (app as any)(req, res);
-}
